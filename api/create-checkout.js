42dcfc6c6b7cec4e90580a090083d8c3576bb79f
makepins <shutupandbookit@gmail.com>
2026-03-18 07:21:57 -0400

    Update create-checkout.js

--- api/create-checkout.js
+++ api/create-checkout.js
@@ -1,13 +1,12 @@
-import Stripe from 'stripe';
+const Stripe = require('stripe');
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
   try {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
     const { userId, email } = req.body;
 
     const session = await stripe.checkout.sessions.create({
@@ -21,7 +20,7 @@ export default async function handler(req, res) {
       ],
       customer_email: email,
       metadata: { userId },
-      success_url: `${req.headers.origin}/success?session_id={CHECKOUT_SESSION_ID}`,
+      success_url: `${req.headers.origin}/?subscribed=true`,
       cancel_url: `${req.headers.origin}/`,
     });
 
